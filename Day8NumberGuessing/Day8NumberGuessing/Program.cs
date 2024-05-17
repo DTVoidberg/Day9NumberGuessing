d97c6ecd09c6301988391baa7d44df0aa9398394
Deividas <davidmabbitt1994@gmail.com>
2024-05-17 20:49:03 +0200

    Add files via upload

---
+++ Day8NumberGuessing/Day8NumberGuessing/Program.cs
@@ -0,0 +1,36 @@
+﻿namespace NumberGuessing;
+
+class Program
+{
+    public static void Main(string[] args)
+    {
+        Random number = new Random();
+
+        int isCorrect = number.Next(0,100);
+
+        bool win = false;
+
+        do
+        {
+            Console.WriteLine("Guess a number between 1 and 100: ");
+            string userInput = Console.ReadLine();
+
+            int i = int.Parse(userInput);
+
+            if (i > isCorrect)
+            {
+                Console.WriteLine("That is too high! Guess again.");
+            }
+            else if (i < isCorrect)
+            {
+                Console.WriteLine("That is too low! Guess again.");
+            }
+            else if (i == isCorrect)
+            {
+                Console.WriteLine("That is correct, You Win!");
+                win = true;
+            }
+
+        } while (win == false);
+    }
+}
